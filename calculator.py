88fb278f43bbe0a82366c7b2af177b87e303243c
C. Todd Lombardo <1851211+iamctodd@users.noreply.github.com>
2025-02-17 15:28:37 +0000

    update for class

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+def main():
+    x = int(input("What's x? "))
+    y = int(input("What's y? "))
+    print("x * y =", multiply(x, y))
+    print("x / y =", divide(x, y))
+
+if __name__ == "__main__":
+    main()
---
+++ test_multiply.py
@@ -0,0 +1,43 @@
+from calculator import multiply
+
+def test_multiply():
+    if multiply(2, 2) != 4:
+            print("Test 1 failed")
+    if multiply(4, 5) != 20:
+            print("Test 2 failed") 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" 
+def test_positive():
+    assert multiply(2, 2) == 4
+    assert multiply(4, 5) == 20
+
+def test_negative():
+    assert multiply(-2, -2) == 4
+    assert multiply(-4, 5) == -20
+    
+def test_zero():
+    assert multiply(0, 3) == 0
+    assert multiply(0, -5) == 0 
+"""
